a03dc3ad946a6f2209322f016d7234d1ad91dbd6
Arkh4in <thomasfalcone.tf@gmail.com>
2019-06-04 21:48:05 +0200

    Add base files

---
+++ Assets/Scripts/Stuff/Weapons/Weapon/Weapon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour, IEquipable
+{
+    [SerializeField]
+    List<ActiveAbility> WeaponAbilities;
+
+    [SerializeField]
+    DamageType Type;
+}
